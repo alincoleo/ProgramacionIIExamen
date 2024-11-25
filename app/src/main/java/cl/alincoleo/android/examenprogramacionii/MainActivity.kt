e113f8e51c67eb97675d444bb06e6d3823583a3b
alincoleo <alvaro.lincoleo@gmail.com>
2024-11-25 18:51:31 -0300

    inicio de proyecto con compose

---
+++ app/src/main/java/cl/alincoleo/android/examenprogramacionii/MainActivity.kt
@@ -0,0 +1,52 @@
+package cl.alincoleo.android.examenprogramacionii
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cl.alincoleo.android.examenprogramacionii.ui.theme.ExamenProgramacionIITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            /*creacion por defecto
+            ExamenProgramacionIITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+
+
+                    )
+                } 
+            } */
+        }
+    }
+}
+
+/* creacion por defecto
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExamenProgramacionIITheme {
+        Greeting("Android")
+    }
+}
+*/
